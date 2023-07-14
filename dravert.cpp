3f218168c918a271d324989c0603cb4323efd4ea
AKSUMRUS <shimorhershow@gmail.com>
2023-07-14 18:44:10 +0100

    feat: added llvm

--- dravert.cpp
+++ dravert.cpp
@@ -1,5 +1,18 @@
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
 #include <string>
 #include <map>
+
+using namespace llvm;
 
 //===----------------------------------------------------------------------===//
 // Lexer
@@ -79,6 +92,7 @@ static int gettok() {
 class ExprAST {
 public:
     virtual ~ExprAST() = default;
+    virtual Value *codegen() = 0;
 };
 
 /// NumberExprAST - Expression class for numeric literals like "1.0".
@@ -86,6 +100,7 @@ class NumberExprAST : public ExprAST {
     double Val;
 public:
     NumberExprAST(double Val): Val(Val) {}
+    Value *codegen() override;
 };
 
 /// VariableExprAST - Expression class for referencing a variable, like "a".
@@ -93,6 +108,7 @@ class VariableExprAST : public ExprAST {
     std::string Name;
 public:
     VariableExprAST(const std::string& Name): Name(Name) {}
+    Value *codegen() override;
 };
 
 /// BinaryExprAST - Expression class for a binary operator.
@@ -103,6 +119,7 @@ public:
     BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
                   std::unique_ptr<ExprAST> RHS)
                   : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+    Value *codegen() override;
 };
 
 /// CallExprAST - Expression class for function calls.
@@ -113,6 +130,7 @@ public:
     CallExprAST(const std::string &Callee,
                 std::vector<std::unique_ptr<ExprAST>> Args)
                 : Callee(Callee), Args(std::move(Args)) {}
+    Value *codegen() override;
 };
 
 /// PrototypeAST - This class represents the "prototype" for a function,
@@ -124,7 +142,7 @@ class PrototypeAST {
 public:
     PrototypeAST(const std::string &Name, std::vector<std::string> Args)
     : Name(Name), Args(std::move(Args)) {}
-
+    Function* codegen();
     const std::string &getName() const { return Name; }
 };
 
@@ -136,6 +154,7 @@ public:
     FunctionAST(std::unique_ptr<PrototypeAST> Proto,
                 std::unique_ptr<ExprAST> Body)
                 : Proto(std::move(Proto)), Body(std::move(Body)) {}
+    Function* codegen();
 };
 
 //===----------------------------------------------------------------------===//
@@ -360,6 +379,134 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
         auto Proto = std::make_unique<PrototypeAST>("", std::vector<std::string>());
         return std::make_unique<FunctionAST>(std::move(Proto), std::move(E));
     }
+    return nullptr;
+}
+
+//===----------------------------------------------------------------------===//
+// Code Generation
+//===----------------------------------------------------------------------===//
+
+static std::unique_ptr<LLVMContext> TheContext;
+static std::unique_ptr<IRBuilder<>> Builder;
+static std::unique_ptr<Module> TheModule;
+static std::map<std::string, Value*> NamedValues;
+
+Value *LogErrorV(const char* Str) {
+    LogError(Str);
+    return nullptr;
+}
+
+Value *NumberExprAST::codegen() {
+    return ConstantFP::get(*TheContext, APFloat(Val));
+}
+
+Value *VariableExprAST::codegen() {
+    // Look this variable up in the function
+    Value* V = NamedValues[Name];
+    if(!V) {
+        return LogErrorV("unknown variable name");
+    }
+    return V;
+}
+
+Value *BinaryExprAST::codegen() {
+    Value *L = LHS->codegen();
+    Value *R = RHS->codegen();
+    if(!L || !R) {
+        return nullptr;
+    }
+
+    switch (Op) {
+        case '+':
+            return Builder->CreateFAdd(L, R, "addtmp");
+        case '-':
+            return Builder->CreateFSub(L, R,"subtmp");
+        case '*':
+            return Builder->CreateFMul(L, R, "multmp");
+        case '<':
+            L = Builder->CreateFCmpULT(L, R, "cmptmp");
+            return Builder->CreateUIToFP(L, Type::getDoubleTy(*TheContext), "boolcmp");
+        default:
+            return LogErrorV("invalid binary operator");
+    }
+}
+
+Value *CallExprAST::codegen() {
+    // Look up the name in the global module table
+    Function *CalleeF = TheModule->getFunction(Callee);
+    if(!CalleeF) {
+        return LogErrorV("unknown function referenced");
+    }
+
+    // If argument mismatch error
+    if(CalleeF->arg_size() != Args.size()) {
+        return LogErrorV("incorrect # arguments passed");
+    }
+
+    std::vector<Value *> ArgsV;
+    for(unsigned i = 0, e = Args.size(); i != e; ++i) {
+        ArgsV.push_back(Args[i]->codegen());
+        if(!ArgsV.back()) {
+            return nullptr;
+        }
+    }
+
+    return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+Function *PrototypeAST::codegen() {
+    // Make the function type:  double(double,double) etc
+    std::vector<Type*> Doubles(Args.size(), Type::getDoubleTy(*TheContext));
+    FunctionType *FT = FunctionType::get(Type::getDoubleTy(*TheContext), Doubles, false);
+    Function *F = Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get());
+
+    // Set names for all arguments
+    unsigned Idx = 0;
+    for(auto &Arg: F->args()) {
+        Arg.setName(Args[Idx++]);
+    }
+
+    return F;
+}
+
+Function *FunctionAST::codegen() {
+    // First, check for an existing function from a previous 'extern' declaration
+    Function *TheFunction = TheModule->getFunction(Proto->getName());
+
+    if(!TheFunction) {
+        TheFunction = Proto->codegen();
+    }
+
+    if(!TheFunction) {
+        return nullptr;
+    }
+
+    if(!TheFunction->empty()) {
+        return (Function *)LogErrorV("function cannot be redefined");
+    }
+
+    // Create a new basic block to start insertion into
+    BasicBlock *BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
+    Builder->SetInsertPoint(BB);
+
+    // Record the function arguments in the NamedValues map
+    NamedValues.clear();
+    for (auto &Arg : TheFunction->args()) {
+        NamedValues[std::string(Arg.getName())] = &Arg;
+    }
+
+    if(Value *RetVal = Body->codegen()) {
+        // Finish off the function
+        Builder->CreateRet(RetVal);
+
+        // Validate the generated code, checking for consistency
+        verifyFunction(*TheFunction);
+
+        return TheFunction;
+    }
+
+    // Error reading body, remove function
+    TheFunction->eraseFromParent();
     return nullptr;
 }
 
